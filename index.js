7c579024460d9a2f3eaac4eb5473c32ed0a95f47
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-11-28 16:21:40 +0000

    making it more readable

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 async function* frstcmfrstsvd(promises) {
-  let resolvers = []
-  let proms = []
+  let resolver = []
+
+  // create an array sortedByFulfillment of pending promises and make available their resolvers in the resolver array
+  let sortedByFulfillment = []
   for (let i = 0; i < promises.length; i++) {
-    proms.push(new Promise((res, rej) => {
-      resolvers.push(res)
+    sortedByFulfillment.push(new Promise((res, rej) => {
+      resolver.push(res)
     }))
   }
 
   promises.forEach((p, i) => {
-    Promise.resolve(p).then(r => {
-      let res = resolvers.shift()
+    Promise.resolve(p).then(r => { 
+      // resolve the first pending promise on the sortedByFulfillment array 
+      let res = resolver.shift()
       res({ value: r, index: i, status: 'fulfilled' })
     }, err => {
-      let res = resolvers.shift()
+      let res = resolver.shift()
       res({ reason: err, index: i, status: 'rejected' })
     })
   })
 
-  for await (let result of proms) {
+  for await (let result of sortedByFulfillment) {
     yield result
   }
 }
