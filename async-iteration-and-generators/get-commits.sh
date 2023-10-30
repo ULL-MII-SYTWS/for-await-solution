3b819ef28a56747acb32beac72f359c86665f95f
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-10-30 16:42:52 +0000

    for-await-solution

--- async-iteration-and-generators/get-commits.sh
+++ async-iteration-and-generators/get-commits.sh
@@ -26,4 +26,4 @@ gh api \
   # -H "Accept: application/vnd.github+json" \
   # -H "Authorization: Bearer $GITHUB_TOKEN" \
   # -H "X-GitHub-Api-Version: 2022-11-28" \
-  # https://api.github.com/repos/torvalds/linux/commits
+  # https://api.github.com/repos/${OWNER}/${REPO}/commits
