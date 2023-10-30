9fe9e66a8b6df4154536fef01cfe7672b6c0a6ec
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-10-30 09:20:23 +0000

    for-await-solution

--- async-iteration-and-generators/async-generators-finally.js
+++ async-iteration-and-generators/async-generators-finally.js
@@ -15,4 +15,13 @@ async function* generateSequence(start, end) {
         process.stdout.write(value+" "); // 1, then 2, then 3, then 4, then 5 (with delay between)
     }
     console.log();
+
+    /* Manually iterate over async generator */
+    generator = generateSequence(6, 10);
+    while (true) {
+        let next = await generator.next(); // In a regular generator we’d use result = generator.next(). In an async generator, we should add await 
+        if (next.done) break;
+        process.stdout.write(next.value+" "); // 6, then 7, then 8, then 9, then 10 (with delay between)
+    }
+    console.log();
   })();
